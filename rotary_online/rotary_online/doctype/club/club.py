16b95a81ddad558e31f85ba58cbb4728f6cbac62
Neil Lasrado <nlasrado@gmail.com>
2020-12-28 02:01:57 +0530

    [feat] Added auto permission creation for AG, AT, DS etc

--- rotary_online/rotary_online/doctype/club/club.py
+++ rotary_online/rotary_online/doctype/club/club.py
@@ -3,8 +3,51 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe.utils import add_years
 from frappe.model.document import Document
 
 class Club(Document):
-	pass
+	def validate(self):
+		if self.assistant_governor:
+			set_user_permission(self.assistant_governor, self.name)
+		if self.assistant_trainer:
+			set_user_permission(self.assistant_trainer, self.name)
+		if self.district_secretary:
+			set_user_permission(self.district_secretary, self.name)
+		if self.chief_coordinator:
+			set_user_permission(self.chief_coordinator, self.name)
+
+def set_user_permission(user, club):
+	existing_perm = frappe.get_all("User Permission", filters={'user':user, 'allow': 'Club', 'for_value': club})
+	if not existing_perm:
+		permission = frappe.new_doc("User Permission")
+		permission.user = user
+		permission.allow = "Club"
+		permission.for_value = club
+		permission.save(ignore_permissions=True)
+	
+def get_timeline_data(doctype, name):
+	project_data = dict(frappe.db.sql('''select unix_timestamp(date(end_time)), count(*)
+		from `tabProject` where club=%s
+			and end_time > date_sub(curdate(), interval 1 year)
+			and docstatus < 2
+			group by date(end_time)''', name))
+
+	meeting_data = dict(frappe.db.sql('''select unix_timestamp(date), count(*)
+		from `tabMeeting` where club=%s
+			and date > date_sub(curdate(), interval 1 year)
+			and docstatus < 2
+			group by date(date)''', name))
+
+	heatmap_data = merge_dicts(project_data, meeting_data)
+	return heatmap_data
+
+def merge_dicts(dict1, dict2):
+	''' Merge dictionaries and add values of common keys'''
+	dict3 = {**dict1, **dict2}
+	for key, value in dict3.items():
+		if key in dict1 and key in dict2:
+			dict3[key] = value + dict1[key]
+
+	return dict3 
