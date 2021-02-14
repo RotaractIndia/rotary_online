b2a8e8056bf48e1b0a4385b87f7871558416f2b6
Neil Lasrado <nlasrado@gmail.com>
2021-02-14 19:48:29 +0530

    [feat] remove dashboard from Club

--- rotary_online/rotary_online/doctype/club/club_dashboard.py
+++ rotary_online/rotary_online/doctype/club/club_dashboard.py
@@ -3,15 +3,5 @@ from frappe import _
 def get_data():
 	return {
 		'heatmap': True,
-		'fieldname': 'club',
-		'transactions': [
-			{
-				'label': 'Activities',
-				'items': ['Meeting', 'Project', 'District Event']
-			},
-			{
-				'label': 'Entity',
-				'items': ['Member', 'Registrations']
-			}
-		]
+		'fieldname': 'club'
 	}
